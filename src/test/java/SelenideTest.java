df8f2d6c65c4449286841ebb78e2448c6b21260f
Viktoriya <vika.vesna.95@mail.ru>
2022-11-22 19:07:02 +0300

    added home work

---
+++ src/test/java/SelenideTest.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideTest {
+
+    @Test
+    void checkThatEnterprisePageOpened(){
+        open("https://github.com");
+        $(withText("Solutions")).hover();
+        $(withText("Enterprise")).click();
+        $("h1").shouldHave(Condition.text("Build like the best"));
+    }
+
+    @Test
+    void dragAndDropTest(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").$("header").shouldHave(Condition.text("B"));
+        $("#column-b").$("header").shouldHave(Condition.text("A"));
+    }
+
+}
